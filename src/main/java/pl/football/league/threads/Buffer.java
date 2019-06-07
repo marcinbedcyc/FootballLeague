2a5efe97c1a00343f22206735826b22ad4018997
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2019-06-07 17:31:00 +0200

    Buffer for consumer and producer

---
+++ src/main/java/pl/football/league/threads/Buffer.java
@@ -0,0 +1,28 @@
+package pl.football.league.threads;
+
+public class Buffer {
+    boolean isBufferFree = true;
+
+    synchronized  boolean get(){
+        if(isBufferFree){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return isBufferFree;
+    }
+
+    synchronized void put(){
+        if(!isBufferFree){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        isBufferFree = false;
+        notify();
+    }
+}
